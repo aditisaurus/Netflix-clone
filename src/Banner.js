167a5a577c57a420bed6680eab95f3b50da559e1
Aditi Singh <aditisinghdeveloper@gmail.com>
2021-08-13 22:26:47 +0530

    Posters

---
+++ src/Banner.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+function Banner () {
+    const [movie, setMovie]= useState([]);
+    useEffect(() => {
+      async function fetchData() {
+
+      }
+      fetchData();
+    }, []);
+    return (
+        <header>
+            
+        </header>
+    )
+}
+
+export default 
+
